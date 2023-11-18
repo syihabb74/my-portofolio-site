e655f29476229e20be024c73523b2135c87a918a
syihabb74 <syihabb74@gmail.com>
2023-11-18 18:43:07 +0700

    my porto site finished

---
+++ main.js
@@ -0,0 +1,13 @@
+import Typed from 'typed.js';
+
+ new Typed('#role', {
+  strings: ['Fullstack Javascript Developer'],
+  typeSpeed: 100,
+  loop: true
+});
+
+new Typed('#portofolio', {
+    strings: ['My Portofolio'],
+    typeSpeed: 100,
+    loop: true
+  });
